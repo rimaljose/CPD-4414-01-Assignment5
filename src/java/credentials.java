7dfba2ff8a1a56856a8a4be233e139bb7680766e
Rimal Jose <rimaljose@gmail.com>
2015-02-23 22:58:24 -0500

    first commit

---
+++ src/java/credentials.java
@@ -0,0 +1,32 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author c0647708
+ */
+public class credentials {
+
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/assignment3";
+            String user = "root";
+            String pass = "";
+            conn = DriverManager.getConnection(jdbc, user, pass);
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(credentials.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+}
