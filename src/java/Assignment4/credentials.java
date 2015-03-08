a350e8e0e0707a3da60a86c4b9f08dcc52a4cb89
Rimal Jose <rimaljose@gmail.com>
2015-03-07 23:47:57 -0500

    second commit

--- src/java/Assignment4/credentials.java
+++ src/java/Assignment4/credentials.java
@@ -22,7 +22,7 @@ public class credentials {
         Connection conn = null;
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            String jdbc = "jdbc:mysql://localhost/assignment3";
+            String jdbc = "jdbc:mysql://localhost/assignment4";
             String user = "root";
             String pass = "";
             conn = DriverManager.getConnection(jdbc, user, pass);
